018c65ebd5d85cfe88a8da74c04b418335c2c01e
Anton Mihaylov <antonmihailov@abv.bg>
2019-08-14 20:06:43 +0100

    Created sorce code

---
+++ src/Hello.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * (c) Anton Mihaylov <anton@amarna.city>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace antonm\HelloWorld;
+
+/**
+ * Class Hello
+ */
+class Hello
+{
+    /**
+     * @return string
+     */
+    public function world()
+    {
+        return 'Hello world!';
+    }
+}
